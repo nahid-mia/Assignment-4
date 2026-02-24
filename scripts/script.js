34ab4aa03df2597820bab052a7c49a3051f69b4c
unknown <moh.nahidmia@gmail.com>
2026-02-24 21:21:11 +0600

    The code is complete with the CSS Now i will only try to optimise the code if i can

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-// showSection function create -- allSection, interviewSection, rejectedSection
-// count id's allCount, interviewCount, rejectedCount
-// interview, rejected btn -- class - addInterview, addRejected
-// The first plan is to create arrays of those sections
-// The second plan is to create functions to update those arrays, to update the counts, to add a universal delete function and simultaneously update those arrays.
-
 
 const allSection = document.querySelectorAll('.allSection');
 const interviewSection = document.querySelectorAll('.interviewSection');
@@ -11,7 +5,38 @@ const rejectedSection = document.querySelectorAll('.rejectedSection');
 const deletedCards = [];
 const interviewCards = [];
 const rejectedCards = [];
+let currentSection = '';
 
+const allSectionBtn = document.getElementById('allSectionBtn');
+const interviewSectionBtn = document.getElementById('interviewSectionBtn');
+const rejectedSectionBtn = document.getElementById('rejectedSectionBtn');
+
+allSectionBtn.addEventListener('click', function () {
+    document.querySelector('.allSection').classList.remove('hidden');
+    document.querySelector('.interviewSection').classList.add('hidden');
+    document.querySelector('.rejectedSection').classList.add('hidden');
+    currentSection = 'allSection';
+    updateAllSectionRightJobCount();
+    showEmptyCard();
+});
+
+interviewSectionBtn.addEventListener('click', function () {
+    document.querySelector('.allSection').classList.add('hidden');
+    document.querySelector('.interviewSection').classList.remove('hidden');
+    document.querySelector('.rejectedSection').classList.add('hidden');
+    currentSection = 'interviewSection';
+    updateInterviewSectionRightJobCount();
+    showEmptyCard();
+});
+
+rejectedSectionBtn.addEventListener('click', function () {
+    document.querySelector('.allSection').classList.add('hidden');
+    document.querySelector('.interviewSection').classList.add('hidden');
+    document.querySelector('.rejectedSection').classList.remove('hidden');
+    currentSection = 'rejectedSection';
+    updateRejectedSectionRightJobCount();
+    showEmptyCard();
+});
 document.addEventListener('click', function (event) {
 
     // I learned and got help from ChatGpt for this part.
@@ -19,88 +44,148 @@ document.addEventListener('click', function (event) {
     if (!card) return;
 
     if (event.target.classList.contains('remove')) {
-        console.log(card);
         card.remove();
+        const allSection = document.querySelector('.allSection');
+
+        allSection.childNodes.forEach(oldCard => {
+            if (oldCard.id === card.id) {
+                oldCard.remove();
+            }
+        });
         const rejectedSection = document.querySelector('.rejectedSection');
         rejectedSection.childNodes.forEach(oldCard => {
             if (oldCard.id === card.id) {
                 oldCard.remove();
-                rejectedCards.splice(rejectedCards.indexOf(card.id), 1);
-                console.log(rejectedCards);
-                updatedCounts(rejectedCards);
+                const index = rejectedCards.indexOf(card.id);
+                if (index !== -1) {
+                    rejectedCards.splice(index, 1);
+                    updatedCounts(rejectedCards);
+                }
             }
         });
         const interviewSection = document.querySelector('.interviewSection');
         interviewSection.childNodes.forEach(oldCard => {
             if (oldCard.id === card.id) {
                 oldCard.remove();
-                interviewCards.splice(interviewCards.indexOf(card.id), 1);
-                console.log(interviewCards);
-                updatedCounts(interviewCards);
+                const index = interviewCards.indexOf(card.id);
+                if (index !== -1) {
+                    interviewCards.splice(index, 1);
+                    updatedCounts(interviewCards);
+                }
             }
         });
-        const allSection = document.querySelector('.allSection');
         const allCount = document.getElementById('allCount');
-        allCount.innerHTML = allSection.childElementCount;
+        allCount.innerHTML = allSection.querySelectorAll('.card').length;
+        const interviewCount = document.getElementById('interviewCount');
+        interviewCount.innerHTML = interviewSection.querySelectorAll('.card').length;
+        const rejectedCount = document.getElementById('rejectedCount');
+        rejectedCount.innerHTML = rejectedSection.querySelectorAll('.card').length;
+        if (currentSection == 'allSection') {
+            updateAllSectionRightJobCount();
+        }
+        else if (currentSection == 'interviewSection') {
+            updateInterviewSectionRightJobCount();
+        }
+        else if (currentSection == 'rejectedSection') {
+            updateRejectedSectionRightJobCount();
+        }
+        showEmptyCard();
+        console.log(rejectedCards);
+        console.log(interviewCards);
+        console.log(interviewSection.childNodes);
     }
 
     if (event.target.classList.contains('addInterview')) {
         interviewSectionUpdate(card);
+        if (currentSection == 'allSection') {
+            updateAllSectionRightJobCount();
+        }
+        else if (currentSection == 'interviewSection') {
+            updateInterviewSectionRightJobCount();
+        }
+        else if (currentSection == 'rejectedSection') {
+            updateRejectedSectionRightJobCount();
+        }
+        showEmptyCard();
     }
 
     if (event.target.classList.contains('addRejected')) {
         rejectedSectionUpdate(card);
+        if (currentSection == 'allSection') {
+            updateAllSectionRightJobCount();
+        }
+        else if (currentSection == 'interviewSection') {
+            updateInterviewSectionRightJobCount();
+        }
+        else if (currentSection == 'rejectedSection') {
+            updateRejectedSectionRightJobCount();
+        }
+        showEmptyCard();
     }
 });
 
 
 
 function interviewSectionUpdate(card) {
-    console.log(card.id);
     if (interviewCards.includes(card.id)) {
         return;
     }
     else {
         const newCard = card.cloneNode(true);
+        card.classList.remove('rejected');
+        card.classList.add('interview');
+        newCard.classList.remove('rejected');
+        newCard.classList.add('interview');
+        card.querySelector('.currentStatus').innerHTML = 'Interview';
+        newCard.querySelector('.currentStatus').innerHTML = 'Interview';
         const rejectedSection = document.querySelector('.rejectedSection');
         rejectedSection.childNodes.forEach(oldCard => {
             if (oldCard.id === card.id) {
                 oldCard.remove();
-                rejectedCards.splice(rejectedCards.indexOf(card.id), 1);
-                console.log(rejectedCards);
-                updatedCounts(rejectedCards);
+                const index = rejectedCards.indexOf(card.id);
+                if (index !== -1) {
+                    rejectedCards.splice(index, 1);
+                    updatedCounts(rejectedCards);
+                }
             }
         });
-
         const interviewSection = document.querySelector('.interviewSection');
         interviewSection.appendChild(newCard);
         interviewCards.push(card.id);
-        console.log(interviewCards);
         updatedCounts(interviewCards);
+        removeEmptyCard(document.querySelector('.interviewSection'));
     }
 }
 
 function rejectedSectionUpdate(card) {
-    console.log(card.id);
     if (rejectedCards.includes(card.id)) {
         return;
     }
     else {
         const newCard = card.cloneNode(true);
+        card.classList.remove('interview');
+        card.classList.add('rejected');
+        newCard.classList.remove('interview');
+        newCard.classList.add('rejected');
+        card.querySelector('.currentStatus').innerHTML = 'Rejected';
+        newCard.querySelector('.currentStatus').innerHTML = 'Rejected';
+
         const interviewSection = document.querySelector('.interviewSection');
         interviewSection.childNodes.forEach(oldCard => {
             if (oldCard.id === card.id) {
                 oldCard.remove();
-                interviewCards.splice(interviewCards.indexOf(card.id), 1);
-                console.log(interviewCards);
-                updatedCounts(interviewCards);
+                const index = interviewCards.indexOf(card.id);
+                if (index !== -1) {
+                    interviewCards.splice(index, 1);
+                    updatedCounts(interviewCards);
+                }
             }
         });
         const rejectedSection = document.querySelector('.rejectedSection');
         rejectedSection.appendChild(newCard);
         rejectedCards.push(card.id);
-        console.log(rejectedCards);
         updatedCounts(rejectedCards);
+        removeEmptyCard(document.querySelector('.rejectedSection'));
     }
 }
 
@@ -112,3 +197,74 @@ function updatedCounts(arr) {
         document.getElementById('interviewCount').innerHTML = arr.length;
     }
 }
+
+
+
+function updateAllSectionRightJobCount() {
+    const innertext = document.querySelector('.allSection').childElementCount;
+    document.getElementById('rightJob').innerText = `${innertext} jobs`;
+}
+
+function updateInterviewSectionRightJobCount() {
+    const innertext = document.querySelectorAll('.interviewSection .card').length;
+    const totalJob = document.querySelectorAll('.allSection .card').length;
+    document.getElementById('rightJob').innerText = `${innertext} jobs of ${totalJob} total`;
+}
+
+function updateRejectedSectionRightJobCount() {
+    const innertext = document.querySelectorAll('.rejectedSection .card').length;
+    const totalJob = document.querySelectorAll('.allSection .card').length;
+    document.getElementById('rightJob').innerText = `${innertext} jobs of ${totalJob} total`;
+}
+
+
+function showEmptyCard() {
+
+    function createEmptyCard() {
+        const div = document.createElement('div');
+        div.className = "emptyCard";
+        div.innerHTML = `
+            <div class="p-5 bg-white rounded-md flex flex-col justify-between items-center">
+                <div class="pt-[60px]"><img src="assets/jobs.png" alt=""></div>
+                <h1 class="text-[24px] font-bold">No Jobs Available</h1>
+                <p class="text-gray-700 pb-[60px]">
+                    Check back soon for new job opportunities
+                </p>
+            </div>`;
+        return div;
+    }
+
+    const all = document.querySelector('.allSection');
+    const interview = document.querySelector('.interviewSection');
+    const rejected = document.querySelector('.rejectedSection');
+
+    if (all.childElementCount === 0 && !all.querySelector('.emptyCard')) {
+        all.appendChild(createEmptyCard());
+    }
+
+    if (interview.childElementCount === 0 && !interview.querySelector('.emptyCard')) {
+        interview.appendChild(createEmptyCard());
+    }
+
+    if (rejected.childElementCount === 0 && !rejected.querySelector('.emptyCard')) {
+        rejected.appendChild(createEmptyCard());
+    }
+}
+
+function removeEmptyCard(section) {
+    const emptyCard = section.querySelector('.emptyCard');
+    // I took help from chatgpt for this part
+    if (emptyCard != null) {
+        emptyCard.remove();
+    }
+}
+
+const sectionBtns = document.querySelectorAll('.sectionBtn');
+sectionBtns.forEach(Btn => {
+    Btn.addEventListener('click', function () {
+        sectionBtns.forEach(btn => btn.classList.remove('active'));
+        Btn.classList.add('active');
+    });
+})
+
+allSectionBtn.click();
